c39bd908b04304b0583130df44bde4e5a6577947
Vanshsuri07 <vanshsuri07@gmai.com>
2025-08-11 18:24:35 +0530

    Setup AuthForm and routing

--- interview-prep/app/layout.tsx
+++ interview-prep/app/layout.tsx
@@ -1,17 +1,18 @@
+
 import type { Metadata } from "next";
-import { Mona_Sans} from "next/font/google";
+import { Mona_Sans } from "next/font/google";
+
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const monaSans = Mona_Sans({
   variable: "--font-mona-sans",
   subsets: ["latin"],
 });
 
-
-
 export const metadata: Metadata = {
   title: "PrepWise",
-  description: "AI Powered platform",
+  description: "An AI-powered platform for preparing for mock interviews",
 };
 
 export default function RootLayout({
@@ -21,10 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body
-        className={`${monaSans.className} antialiased`}
-      >
+      <body className={`${monaSans.className} antialiased pattern `}
+       >
         {children}
+
+       <Toaster />
       </body>
     </html>
   );

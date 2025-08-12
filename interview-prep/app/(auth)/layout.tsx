180315fb8941f1bd582003c917a3e823af088cee
Vanshsuri07 <vanshsuri07@gmai.com>
2025-08-13 01:39:06 +0530

    Fixing Error

--- interview-prep/app/(auth)/layout.tsx
+++ interview-prep/app/(auth)/layout.tsx
@@ -1,15 +1,12 @@
 import { ReactNode } from "react";
-import { redirect } from 'next/navigation';
-import { isAuthenticated } from '@/lib/actions/auth.actions';
+import { redirect } from "next/navigation";
 
-
-
+import { isAuthenticated } from "@/lib/actions/auth.actions";
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
-  
-    const isUserAuthenticated = await isAuthenticated();
-  
-    if (!isUserAuthenticated) redirect('/sign-in');
+  const isUserAuthenticated = await isAuthenticated();
+  if (isUserAuthenticated) redirect("/");
+
   return <div className="auth-layout">{children}</div>;
 };
 
--- interview-prep/app/(root)/layout.tsx
+++ interview-prep/app/(root)/layout.tsx
@@ -1,26 +1,26 @@
-import { isAuthenticated } from '@/lib/actions/auth.actions';
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { ReactNode } from 'react'
+import Link from "next/link";
+import Image from "next/image";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
 
-import { redirect } from 'next/navigation';
-    
-const RootLayout = async ({children} : {children : ReactNode}) => {
+import { isAuthenticated } from "@/lib/actions/auth.actions";
 
+const Layout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect("/sign-in");
 
-  if (!isUserAuthenticated) redirect('/sign-in');
   return (
-    <div className='root-layout'>
+    <div className="root-layout">
       <nav>
-        <Link href="/" className='flex items-center gap-2'>
-          <Image src="/logo.svg" alt="Logo" height={32} width={38} />
-          <h2 className='text-primary-100'>Prepwise</h2>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+          <h2 className="text-primary-100">PrepWise</h2>
         </Link>
       </nav>
-      {children}
-      </div>
-  )
-}
 
-export default RootLayout
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
